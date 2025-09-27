4a67e389b37ae33d0c75be98f2bf6b4aee3b8d34
lee gaeun <dlrkdmsdlaa00@gmail.com>
2025-09-27 18:26:27 +0900

    수정1

--- jpademo/src/main/java/com/likelion/jpademo/entity/Member.java
+++ jpademo/src/main/java/com/likelion/jpademo/entity/Member.java
@@ -7,25 +7,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor @Builder
+@Getter
+@NoArgsConstructor
 public class Member {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     private String nickname;
 
-    // 주인 아님 (읽기/탐색용)
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
 
-    // 양방향 편의 메서드
+    @Builder
+    public Member(String nickname) {
+        this.nickname = nickname;
+        this.posts = new ArrayList<>();
+    }
+
     public void addPost(Post p) {
         posts.add(p);
         p.setMember(this);
     }
 }
-
-
-//패키지 이름이 다를 때, alt+enter 하면 자동으로 알맞게 설정해줌
--- jpademo/src/main/java/com/likelion/jpademo/entity/Post.java
+++ jpademo/src/main/java/com/likelion/jpademo/entity/Post.java
@@ -4,21 +4,32 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor @Builder
+@Getter
+@NoArgsConstructor
 public class Post {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     private String title;
 
+    @Setter
     @Lob
     private String content;
 
-    // ⚠️ 연관관계 주인 (FK 보유)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
-}
 
+    @Builder
+    public Post(String title, String content, Member member) {
+        this.title = title;
+        this.content = content;
+        this.member = member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+}
